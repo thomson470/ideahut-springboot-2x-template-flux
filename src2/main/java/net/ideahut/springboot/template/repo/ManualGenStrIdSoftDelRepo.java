10e05b617a347c08089bab6017ae9e9c07154f56
Thomson <thomson470@gmail.com>
2024-08-19 12:49:35 +0700

    Upgrade ke Ideahut versi 3.0.0

--- src2/main/java/net/ideahut/springboot/template/repo/ManualGenStrIdSoftDelRepo.java
+++
@@ -1,7 +0,0 @@
-package net.ideahut.springboot.template.repo;
-
-import net.ideahut.springboot.template.entity.app.ManualGenStrIdSoftDel;
-
-public interface ManualGenStrIdSoftDelRepo extends SoftDeleteRepository<ManualGenStrIdSoftDel, String> {
-
-}
